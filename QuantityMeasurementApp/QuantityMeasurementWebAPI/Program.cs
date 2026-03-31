64d45f6f1f5b2d6fb8ec9ec013228bccf246659d
Sankalp Agarwal <sankalpagarwal8057@gmail.com>
2026-03-31 10:45:46 +0530

    [Sankalp_Agarwal] Refactor the project for Render deployment

--- QuantityMeasurementApp/QuantityMeasurementWebAPI/Program.cs
+++ QuantityMeasurementApp/QuantityMeasurementWebAPI/Program.cs
@@ -167,7 +167,8 @@ try
                         "http://localhost:3001",
                         "http://127.0.0.1:5500",
                         "http://localhost:5500",
-                        frontendUrl
+                        frontendUrl,
+                        "https://quantitymeasurementapp-frontend.onrender.com"
                     )
                     .AllowAnyMethod()
                     .AllowAnyHeader()
